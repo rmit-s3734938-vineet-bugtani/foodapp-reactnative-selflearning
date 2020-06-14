f2baa1405d466b7f6be98a9d6a87e363e605f1d0
Vineet <s3734938@student.rmit.edu.au>
2020-06-15 01:59:34 +1000

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import RootStack from '../foodapp/src/RootStack';
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
---
+++ src/Home.js
@@ -0,0 +1,73 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Dimensions,
+  Text,
+  SafeAreaView,
+} from 'react-native';
+import ScrollableTabView, {
+  DefaultTabBar,
+} from 'react-native-scrollable-tab-view';
+import All from '../src/home_child/All';
+import Menu from '../src/home_child/Menu';
+import Popular from '../src/home_child/Popular';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <ImageBackground
+            source={require('./asset/header.png')}
+            style={styles.imageBackground}
+            resizeMode="contain">
+            <Text style={styles.title}>HOME</Text>
+          </ImageBackground>
+        </View>
+        <View style={styles.tabbar}>
+          <ScrollableTabView
+            initialPage={0}
+            tabBarActiveTextColor={'green'}
+            tabBarUnderlineStyle={{backgroundColor: 'green'}}
+            renderTabBar={() => <DefaultTabBar />}>
+            <All tabLabel="All" props={this.props} />
+            <Menu tabLabel="Menu" props={this.props} />
+            <Popular tabLabel="Popular" props={this.props} />
+          </ScrollableTabView>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+const width = Dimensions.get('screen').width;
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    marginTop: 0,
+  },
+  tabbar: {
+    flex: 1,
+    paddingHorizontal: 30,
+    marginTop: -50,
+  },
+  imageBackground: {
+    width: width * 0.4,
+    height: width * 0.4,
+    alignItems: 'center',
+    position: 'relative',
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+    position: 'absolute',
+    marginTop: 20,
+  },
+});
